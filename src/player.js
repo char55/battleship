e71dbb2f71f4082badddcf878f6829aa2618c5d4
char55 <haras.charlotte@gmail.com>
2018-12-07 22:44:52 +0000

    init game board and 2 players

---
+++ src/player.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Board from './board.js'
+
+class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: 'X',
+      round: 1,
+    };
+    this.nextPlayer = this.nextPlayer.bind(this);
+    this.newRound = this.newRound.bind(this);
+  }
+
+  nextPlayer() {
+    const Allplayers = ['X', 'O'];
+    this.setState({player: Allplayers[this.state.round % 2]})
+    this.newRound(this.state.round)
+  }
+  
+  newRound() {
+    this.setState({round: this.state.round + 1})
+  }
+
+  render() {
+    const status = `Next player: ${this.state.player}`;
+    return (
+      <div>
+      <div className="status">{status}</div>
+        <Board player={this.state.player} nextPlayer={this.nextPlayer} round={this.state.round}/>
+      </div>
+    );
+  }
+}
+
+export default Player;
